9e4f51af26a4a6cea5a9e522d0480806417b3de5
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-03-01 10:56:03 +0100

    Added a pretty cool method and stuff and things

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-def rasmusmethod(x, y):
-	print(x*y+2)
+# ramusmethod for life
+
+def recur(indict={1:'hej'}):
+	print(indict)
+	indict[i] = i * 2
 
 for i in range(100):
-	print(rasmusmethod(i, i*2))
-
-#Stop that kevin! REEE
+	recur()
